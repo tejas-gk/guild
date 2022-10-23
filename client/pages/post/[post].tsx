39528dee4761169748125e1d7cad946c7bf9514a
tejas-gk <tejgankri@outlook.com>
2022-10-23 22:10:46 +0530

    fuck somebody help me SOS

--- client/pages/post/[post].tsx
+++ client/pages/post/[post].tsx
@@ -1,24 +1,23 @@
 import {useState,useEffect} from 'react'
 import axios from 'lib/axios'
 import {useRouter} from 'next/router'
-export default function Posts(id:number) {
+export default function Posts() {
+    const [post, setPost] = useState(null)
     const router = useRouter()
-    // useEffect(() => {
-        //     axios.get(`/post/${id}`).then(res => {
-            //         setPost(res.data)
-            //     })
-            // },[])
-            
-            const getAParticularPost:any = async (id) => {
-                axios.get("/post/1").then((response) => {
-                    setPost(response.data);
-                });
-            };
-            const [post,setPost] = useState(getAParticularPost(id))
+    let id:any=router.query.post 
+
+    useEffect(() => {
+        axios.get(`/post/${id}`).then(res => {
+        setPost(res.data)
+        })
+        console.log(router.query,id)
+    }, [])
+
 
     return (
         <div>
         <h1>Post</h1>
+        
         {post && <p>{post.post}</p>}
         </div>
     )
