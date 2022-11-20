bea90e374364093647784519f2d5732b9b5fa57c
tejas-gk <tejgankri@outlook.com>
2022-11-20 23:10:15 +0530

    fix:is following

--- server/routes/api.php
+++ server/routes/api.php
@@ -75,7 +75,7 @@ Route::post('follow/{id}',[App\Http\Controllers\Utils\FollowController::class,'f
 Route::get('vote/{id}',[VoteController::class,'getVoteCount'])->middleware('auth:sanctum');
 Route::get('follower/{id}',[App\Http\Controllers\Utils\FollowController::class,'getFollowersCount'])->middleware('auth:sanctum');
 Route::get('following/{id}',[App\Http\Controllers\Utils\FollowController::class,'getFollowingsCount'])->middleware('auth:sanctum');
-
+Route::get('is-following/{id}',[App\Http\Controllers\Utils\FollowController::class,'isFollowing'])->middleware('auth:sanctum');
 Route::get('user/{id}',[App\Http\Controllers\User\UserController::class,'show']);
 
 
